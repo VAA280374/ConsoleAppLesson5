09046574f3d72f367d734e8a957b7ae7e67f6d66
User <User@VAA-NOTE>
2021-12-02 07:22:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLesson5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = new double[7];
+            double vSum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите элемент {0} массива ", i);
+                array[i] = Convert.ToDouble(Console.ReadLine());
+                vSum = vSum + array[i];
+            }
+            Console.WriteLine("Длина массива = {0}", array.Length);
+            vSum = vSum / array.Length;
+            Console.WriteLine("Среднее арифметическое массива {0}", vSum);
+            
+            Console.ReadKey();
+        }
+    }
+}
